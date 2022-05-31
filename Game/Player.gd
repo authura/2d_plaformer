234b5781569b9172e0ea4985c0327edadaae5dff
authura <rhoferdevs@gmail.com>
2022-05-31 11:21:31 -0400

    added friction/acceleration, fall animation

--- Game/Player.gd
+++ Game/Player.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = -550
 var motion = Vector2()
 
@@ -10,25 +11,33 @@ var motion = Vector2()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("run")
 	else:
-		motion.x = 0
+		friction = true
 		$Sprite.play("idle")
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 			
 	else:
-		$Sprite.play("jump")
+		if motion.y < 0:
+			$Sprite.play("jump")
+		else:
+			$Sprite.play("fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 	
 	motion = move_and_slide(motion, UP)
 	pass
