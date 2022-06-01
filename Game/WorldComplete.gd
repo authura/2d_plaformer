9e3376db3362c29a0f8962f021cc9ca2355b5533
authura <rhoferdevs@gmail.com>
2022-05-31 23:11:33 -0400

    added area collider for transfer to second level

---
+++ Game/WorldComplete.gd
@@ -0,0 +1,10 @@
+#WorldComplete.gd
+extends Area2D
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	
+	for body in bodies:
+		if body.name == "Player":
+			get_tree().change_scene("res://World2.tscn")
+
